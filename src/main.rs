94d08bb00374e3c484077124b3139efad41e8a7b
jeremymarch <jmarch@gradcenter.cuny.edu>
2022-09-04 00:03:34 -0400

    better variable names and ui details like focus/etc

--- src/main.rs
+++ src/main.rs
@@ -18,43 +18,42 @@ use std::sync::{Arc, Mutex};
 const APP_ID: &str = "com.philolog.hc-gtk";
 
 fn build_ui(app: &Application) {
-    let text_view: gtk::TextView = gtk::TextView::new();
-    text_view.set_wrap_mode(gtk::WrapMode::Word);
-    text_view.set_cursor_visible(true);
-    text_view.set_justification(gtk::Justification::Center);
+    let changed_form_tv_orig: gtk::TextView = gtk::TextView::new();
+    changed_form_tv_orig.set_wrap_mode(gtk::WrapMode::Word);
+    changed_form_tv_orig.set_cursor_visible(false);
+    changed_form_tv_orig.set_editable(false);
+    changed_form_tv_orig.set_justification(gtk::Justification::Center);
     let margin = 10;
-    text_view.set_left_margin(margin);
-    text_view.set_top_margin(margin);
-    text_view.set_right_margin(margin);
-    text_view.set_bottom_margin(margin);
+    changed_form_tv_orig.set_left_margin(margin);
+    changed_form_tv_orig.set_top_margin(margin);
+    changed_form_tv_orig.set_right_margin(margin);
+    changed_form_tv_orig.set_bottom_margin(margin);
 
-    let scrolled_window = gtk::ScrolledWindow::builder()
+    let changed_form_scrolled_window = gtk::ScrolledWindow::builder()
         .hscrollbar_policy(gtk::PolicyType::Never) // Disable horizontal scrolling
         .min_content_width(360)
         .vexpand(true)
-        .child(&text_view)
+        .child(&changed_form_tv_orig)
         .build();
 
-    let text_view2: gtk::TextView = gtk::TextView::new();
-    text_view2.set_wrap_mode(gtk::WrapMode::Word);
-    text_view2.set_cursor_visible(false);
-    text_view2.set_justification(gtk::Justification::Center);
-    text_view2.set_left_margin(margin);
-    text_view2.set_top_margin(margin);
-    text_view2.set_right_margin(margin);
-    text_view2.set_bottom_margin(margin);
-    text_view2.set_editable(false);
-
-    //text_view2.buffer().set_text("\nφέρω");
+    let starting_form_tv_orig: gtk::TextView = gtk::TextView::new();
+    starting_form_tv_orig.set_wrap_mode(gtk::WrapMode::Word);
+    starting_form_tv_orig.set_cursor_visible(false);
+    starting_form_tv_orig.set_justification(gtk::Justification::Center);
+    starting_form_tv_orig.set_left_margin(margin);
+    starting_form_tv_orig.set_top_margin(margin);
+    starting_form_tv_orig.set_right_margin(margin);
+    starting_form_tv_orig.set_bottom_margin(margin);
+    starting_form_tv_orig.set_editable(false);
         
-    let scrolled_window2 = gtk::ScrolledWindow::builder()
+    let starting_form_scrolled_window = gtk::ScrolledWindow::builder()
         .hscrollbar_policy(gtk::PolicyType::Never) // Disable horizontal scrolling
         .min_content_width(360)
         .vexpand(true)
-        .child(&text_view2)
+        .child(&starting_form_tv_orig)
         .build();
 
-    let label = gtk::Label::new(Some(""));
+    let change_label = gtk::Label::new(Some(""));
     //label.set_markup("First <b>Singular</b> Present <b>Active</b> Indicative");
 
     let correct_label = gtk::Label::new(Some(""));
@@ -73,9 +72,9 @@ fn build_ui(app: &Application) {
         .title_widget(&adw::WindowTitle::new("Hoplite Challenge", ""))
         .build(),
     );
-    vbox.append(&scrolled_window2);
-    vbox.append(&label);
-    vbox.append(&scrolled_window);
+    vbox.append(&starting_form_scrolled_window);
+    vbox.append(&change_label);
+    vbox.append(&changed_form_scrolled_window);
     vbox.append(&correct_label);
     vbox.append(&button);
 
@@ -83,8 +82,8 @@ fn build_ui(app: &Application) {
     if let Ok(mut ch) = chooser.lock() {
         ch.set_reps_per_verb(4);
     }
-    let tv2 = text_view2.clone();
-    let tv1 = text_view.clone();
+    let starting_form_tv = starting_form_tv_orig.clone();
+    let changed_form_tv = changed_form_tv_orig.clone();
     button.connect_clicked(move |button| {
         if let Ok(mut ch) = chooser.lock() {
             if ch.history.len() == 0 {
@@ -92,20 +91,26 @@ fn build_ui(app: &Application) {
             }
 
             if button.label().unwrap() == "Submit" {
-                let answer = tv1.buffer().text(&tv1.buffer().start_iter(), &tv1.buffer().end_iter(), false);
-                let prev1 = &ch.history[ch.history.len() - 1]; //call here before calling next_form()
-                let form = prev1.get_form(false).unwrap().last().unwrap().form.to_string();
+                changed_form_tv.set_editable(false);
+                changed_form_tv.set_cursor_visible(false);
+                let answer = changed_form_tv.buffer().text(&changed_form_tv.buffer().start_iter(), &changed_form_tv.buffer().end_iter(), false);
+                let prev_vf = &ch.history[ch.history.len() - 1]; //call here before calling next_form()
+                let answer_correct = prev_vf.get_form(false).unwrap().last().unwrap().form.to_string();
+                
                 if let Ok(vf) = ch.next_form(Some(&answer)) {
-                    let is_correct = vf.1;
-                    if let Some(ic) = is_correct {
-                        if ic {
+                    let is_correct_option = vf.1;
+                    if let Some(is_correct) = is_correct_option {
+                        if is_correct {
                             println!("correct");
                             correct_label.set_markup("<span foreground=\"green\">correct</span>");
                         }
                         else {
                             println!("incorrect");
-                            
-                            correct_label.set_markup(format!("<span foreground=\"red\">incorrect: {}</span>", form).as_str());
+                            correct_label.set_markup(format!("<span foreground=\"red\">incorrect: {}</span>", answer_correct).as_str());
+
+                            //new verb!
+                            // ch.verb_counter = 0;
+                            // _ = ch.next_form(None);
                         }
                     }
                     button.set_label("Continue");
@@ -115,24 +120,20 @@ fn build_ui(app: &Application) {
                 button.set_label("Submit");
 
                 println!("counter: {}, reps: {}", ch.verb_counter, ch.reps_per_verb);
-                // if ch.verb_counter == 1 {
-                //     _ = ch.next_form(None); //change to...
-                //     //ch.verb_counter = 6;
-                //     println!("change");
-                // }
 
-                let prev1 = &ch.history[ch.history.len() - 2];
-                let form = prev1.get_form(false).unwrap().last().unwrap().form.to_string();
+                let starting_vf = &ch.history[ch.history.len() - 2];
+                let starting_form = starting_vf.get_form(false).unwrap().last().unwrap().form.to_string();
 
-                let prev = &ch.history[ch.history.len() - 1];
-                //let prev_f = prev.get_form(false).unwrap().last().unwrap().form.to_string();
-                //println!("prev {}", prev_f);
+                let changed_vf = &ch.history[ch.history.len() - 1];
 
-                label.set_text(format!("{:?} {:?} {:?} {:?} {:?}", prev.person, prev.number, prev.tense, prev.mood, prev.voice).as_str());
+                change_label.set_text(format!("{:?} {:?} {:?} {:?} {:?}", changed_vf.person, changed_vf.number, changed_vf.tense, changed_vf.mood, changed_vf.voice).as_str());
 
                 correct_label.set_markup("");
-                tv2.buffer().set_text(&form);
-                tv1.buffer().set_text("");
+                starting_form_tv.buffer().set_text(&starting_form);
+                changed_form_tv.buffer().set_text("");
+                changed_form_tv.set_editable(true);
+                changed_form_tv.set_cursor_visible(true);
+                changed_form_tv.grab_focus();
             }
         }
     });
@@ -148,7 +149,7 @@ fn build_ui(app: &Application) {
     //https://github.com/avranju/spacer-win/blob/65779df6e5a3884cc499cc2c48cd1348a7b4d63e/src/main.rs
     //https://stackoverflow.com/questions/72796392/how-to-bind-a-key-press-to-a-speccific-action-in-gtk4
     let evk = EventControllerKey::new();
-    let tv = text_view.clone();
+    let tv = changed_form_tv_orig.clone();
     evk.connect_key_pressed(move |_evck, key, _code, _state| {
         
         let diacritic_option:Option<u32> = match key {
@@ -218,7 +219,7 @@ fn build_ui(app: &Application) {
 
         Inhibit(false)
     });
-    text_view.add_controller(&evk);
+    changed_form_tv_orig.add_controller(&evk);
 
     window.present();
 }
